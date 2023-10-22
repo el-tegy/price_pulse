14ba57dae624b7c91befc9225c0c436a9b0c3b31
el-tegy <elisee.tegue@groupe-esigelec.org>
2023-10-22 09:42:08 +0200

    🏜  perf: round the difference between the 02 prices to 2 digits

--- price_pulse_scraper/price_pulse_scraper/price_pulse_comparator.py
+++ price_pulse_scraper/price_pulse_scraper/price_pulse_comparator.py
@@ -9,7 +9,7 @@ shopB_silver.rename(columns={'price': 'price_idealo'}, inplace=True)
 # merge the two dataframes on the series column
 price_pulse_comparator = shopA_silver.merge(shopB_silver, on='series', how='inner').rename(columns={'link_x': 'link_ipixmania', 'link_y': 'link_idealo'})
 # absolute difference between the two prices
-price_pulse_comparator['price_diff'] = abs(price_pulse_comparator['price_pixmania'] - price_pulse_comparator['price_idealo'])
+price_pulse_comparator['price_diff'] = round(abs(price_pulse_comparator['price_pixmania'] - price_pulse_comparator['price_idealo']), 2)
 # recommend the cheapest price
 price_pulse_comparator['recomended_store'] = price_pulse_comparator.apply(lambda row: 'Pixmania' if row['price_pixmania'] < row['price_idealo'] else 'Idealo', axis=1)
 # order the columns in this order ['series', 'price_pixmania', 'price_idealo', 'price_diff', 'recomended_store', 'link_ipixmania', 'link_idealo']]  
